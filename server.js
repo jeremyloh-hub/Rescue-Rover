70ebbf4d47457de9132884d3d18a07bab8140dfb
jeremyloh-hub <eziorain@gmail.com>
2023-04-17 10:35:10 +0800

    update server.js

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require("path");
 require("dotenv").config();
 require("./config/database");
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "dist")));
 
 const dogRouter = require("./routes/dogs.tsx");
 
